9a8fa449e16b2f378cb778f3cebdb6b2302c3851
srinivasaguru1 <srinivasaguru301@gmail.com>
2025-07-30 15:28:12 +0530

    code file updated

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import os
 import json
 import sys
 import boto3
-print("imported successfully...")
 
-prompt = """
+print("impotred successfully...")
 
-    you are a smart assistant so please let me know what is machine learning in smartest way?
+prompt="""
 
-
+        you are a cricket expert now just tell me when RCB will win the IPL?
 """
-bedrock=boto3.client(service_name="bedrock-runtime")
+
+bedrock=boto3.client(service_name="bedrock-runtime",region_name="us-east-1")
+
 payload={
     
     "prompt": "[INST]"+prompt+"[/INST]",
@@ -19,15 +20,17 @@ payload={
     "top_p":0.9
 }
 
+body=json.dumps(payload)
+model_id="meta.llama3-8b-instruct-v1:0"
 
-body= json.dumps(payload)
-model_id="meta.llama2-70b-chat-v1"
 
-response = bedrock.invoke_model(
+response=bedrock.invoke_model(
     body=body,
-    model_id=model_id,
+    modelId=model_id,
     accept="application/json",
-    content_type="application/json")
+    contentType="application/json"
+)
+
 response_body=json.loads(response.get("body").read())
-response_test= response_body["generation"]
-print(response_test)
+response_text=response_body["generation"]
+print(response_text)
