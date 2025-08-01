b99aae21eed7f916eb542fde29b82a63154567e9
srinivasaguru1 <srinivasaguru301@gmail.com>
2025-08-01 08:26:56 +0530

    code updated

--- QASystem/app.py
+++ QASystem/app.py
@@ -1,4 +1,7 @@
-import os
-import json
-import sys
-import boto3
+from langchain_community.embeddings import BedrockEmbedding
+
+from langchain.llms.bedrock import Bedrock
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import retrieval_qa
+from langchain.vectorstores import FAISS
